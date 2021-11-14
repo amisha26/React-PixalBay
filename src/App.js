dff2268eac6a108d59b5e125a5c4bc3120f27a5f
amisha26 <amisha8299@gmail.com>
2021-11-14 20:39:41 +0530

    app.s changes done

--- src/App.js
+++ src/App.js
@@ -8,6 +8,16 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [term, setTerm] = useState('');
 
+  useEffect(() => {
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
+      .then(res => res.json())
+      .then(data => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch(err => console.log(err));
+  }, [term]);
+
   return (
     <div className="container mx-auto">
      
