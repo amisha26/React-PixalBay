3cf6c384ff4b1c9e2be24bd27e35d24ebfe210c9
amisha26 <amisha8299@gmail.com>
2021-11-14 20:37:51 +0530

    app.s

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import ImageCard from './components/ImageCard';
+import ImageSearch from './components/ImageSearch';
 
 
 function App() {
-
-
+  
   return (
     <div className="container mx-auto">
-
+     
     </div>
   );
 }
