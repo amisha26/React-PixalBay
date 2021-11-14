bb35348cf3fdd8aadd27419d2870edaa442a7a89
amisha26 <amisha8299@gmail.com>
2021-11-14 20:39:10 +0530

    app.s

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import ImageSearch from './components/ImageSearch';
 
 
 function App() {
-  
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
   return (
     <div className="container mx-auto">
      
