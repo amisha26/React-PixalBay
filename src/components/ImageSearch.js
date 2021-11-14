17a74f6d963775471a751903f8c9f8cbe374972f
amisha26 <amisha8299@gmail.com>
2021-11-14 20:43:12 +0530

    imageSearch.js

---
+++ src/components/ImageSearch.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+const ImageSearch = ({ searchText }) => {
+
+
+
+  return (
+    <div className='max-w-sm rounded overflow-hidden my-10 mx-auto'>
+    
+		</div>
+  )
+}
+
+export default ImageSearch;
